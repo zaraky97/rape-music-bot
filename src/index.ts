6e212c323452d3e4f1a68bef63d3006bf3bf8c46
sasagawa <sasagawasan@hotmail.co.jp>
2022-05-01 01:24:07 +0900

    ヘルプコマンド追加

--- src/index.ts
+++ src/index.ts
@@ -28,14 +28,46 @@ client.on('ready', () => {
   console.log(`${client.user?.tag} でログインしています。`);
 });
 
-client.on('messageCreate', (msg) => {
+client.on('messageCreate', async (msg) => {
+  const userId = msg.member?.user.id;
+  const username = msg.member?.user.username;
   if (msg.content === 'Hello') msg.reply('Hi');
   if (msg.content === 'こんにちは') {
     msg.channel.send('オイッスー！');
   }
+  if (msg.content === '!music-help') {
+    msg.channel.send(
+      '曲の登録・更新\n```!music-update youtubeのurl 始まりの時間(s) 動画の長さ(s)\n例）!music-update https://www.youtube.com/watch?v=upODO6OuOOk 9 15```'
+    );
+    msg.channel.send('自分が登録した曲の確認\n```!music-me```');
+    msg.channel.send('サーバーに登録されている曲の確認\n```!music-list```');
+  }
+  if (msg.content === '!music-me') {
+    if (!userId) return;
+    const storeMusicdata = await getMusicdata(db, userId);
+    if (!storeMusicdata) {
+      msg.channel.send('登場曲が見つからないッス');
+    } else {
+      msg.channel.send(
+        `${username}の登場曲: ${storeMusicdata.urls} ${storeMusicdata.start} ${storeMusicdata.duration}`
+      );
+    }
+  }
+  if (msg.content.includes('!music-update')) {
+    const msgArray = msg.content.split(' ');
+    const msgDataObject = {
+      urls: [msgArray[1]],
+      start: msgArray[2] ? +msgArray[2] : 0,
+      duration: msgArray[3] ? +msgArray[3] : 15,
+    };
+    if (!userId || !username) return;
+    if (username === 'rape-music-bot') return;
+    updateMusic(db, userId, username, msgDataObject);
+    msg.channel.send(`更新! url: ${msgArray[1]}`);
+  }
 });
 
-client.on('voiceStateUpdate', (oldMember, newMember) => {
+client.on('voiceStateUpdate', async (oldMember, newMember) => {
   if (
     newMember.member?.user.username === 'rape-music-bot' ||
     oldMember.channelId === newMember.channelId
