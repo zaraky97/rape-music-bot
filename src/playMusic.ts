2c6a8e77344815b2df197a9cf3ed107fdfe96cd9
zaraky97 <sasagawasan@hotmail.co.jp>
2022-05-11 00:50:37 +0900

    durationを秒に修正

--- src/playMusic.ts
+++ src/playMusic.ts
@@ -18,35 +18,39 @@ export function playMusic(
   }
 ) {
   try {
-    const subscriber = connection.subscribe(audioPlayer);
-    const stream = ytdl(ytdl.getURLVideoID(musicdata.urls[0]), {
+    connection.subscribe(audioPlayer);
+    const readableStream = ytdl(ytdl.getURLVideoID(musicdata.urls[0]), {
       filter: 'audioonly',
       highWaterMark: 1 << 62,
       liveBuffer: 1 << 62,
       dlChunkSize: 0, //disabling chunking is recommended in discord bot
       quality: 'lowestaudio',
     });
+    // const { stream, type } = await demuxProbe(readableStream);
+    // console.log(stream, type);
     let editedSong = undefined;
     if (musicdata.start || musicdata.duration) {
       fluentFfmpeg.setFfmpegPath(ffmpeg_static);
-      editedSong = fluentFfmpeg({ source: stream })
+      editedSong = fluentFfmpeg({ source: readableStream })
         .toFormat('mp3')
         .setStartTime(musicdata.start ?? 0)
-        .setDuration(musicdata.duration ? musicdata.duration * 1000 : 10000);
+        .setDuration(musicdata.duration ? musicdata.duration : 10);
     } else {
-      editedSong = stream;
+      editedSong = readableStream;
     }
     const resource = createAudioResource(editedSong as any, {
       inputType: StreamType.Arbitrary,
       inlineVolume: true,
     });
-    resource.volume?.setVolume(0.3);
+    resource.volume?.setVolume(0.5);
     audioPlayer.play(resource);
-    audioPlayer.addListener('stateChange', (oldState: any, newState: any) => {
-      if (newState.status === 'idle') {
-        subscriber?.unsubscribe();
-      }
-    });
+    // audioPlayer.on('stateChange', (oldState: any, newState: any) => {
+    //   console.log(oldState.status, newState.status);
+    //   if (newState.status === 'idle') {
+    //     subscriber?.unsubscribe();
+    //     audioPlayer.stop();
+    //   }
+    // });
   } catch (e) {
     console.log(e, 'play music');
   }
